50a8686e48536035582f5cc79246a295c6485d24
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-30 17:37:57 +0530

    feat: Add Constants.kt for shared app constants.

---
+++ app/src/main/java/com/evaluation/mangascope/core/Constants.kt
@@ -0,0 +1,9 @@
+package com.evaluation.mangascope.core
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object Constants {
+    object Preferences {
+        val SIGNED_USER_EMAIL = stringPreferencesKey("signed_user_email")
+    }
+}
