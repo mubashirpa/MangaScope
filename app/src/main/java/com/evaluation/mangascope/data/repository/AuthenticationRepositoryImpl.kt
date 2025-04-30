bac75676c398b6c69dd73a46bc3bfbd9dae486fa
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-30 17:36:20 +0530

    Refactor: Implement data persistence for sign-in status and retrieve user by email.
    -   Add `signInPreferencesFlow` to `AuthenticationRepository` to observe user sign-in status.
    -   Implement `signIn` to store user credentials locally and update sign-in status.
    -   Implement `getUserByEmail` to fetch a specific user from the local database.
    -   Add dataStore dependency to store signed-in user email.
    -   Create DataStore instance.
    -   Add dataStore implementation to save signed in email.

--- app/src/main/java/com/evaluation/mangascope/data/repository/AuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/evaluation/mangascope/data/repository/AuthenticationRepositoryImpl.kt
@@ -1,18 +1,50 @@
 package com.evaluation.mangascope.data.repository
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.evaluation.mangascope.core.Constants
 import com.evaluation.mangascope.data.local.dao.UserDao
 import com.evaluation.mangascope.data.local.entity.UserEntity
+import com.evaluation.mangascope.domain.model.preferences.SignInPreferences
 import com.evaluation.mangascope.domain.repository.AuthenticationRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.io.IOException
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "sign_in_preferences")
 
 class AuthenticationRepositoryImpl(
     private val userDao: UserDao,
+    private val dataStore: DataStore<Preferences>,
 ) : AuthenticationRepository {
+    override val signInPreferencesFlow: Flow<SignInPreferences>
+        get() =
+            dataStore.data
+                .catch { exception ->
+                    if (exception is IOException) {
+                        emit(emptyPreferences())
+                    } else {
+                        throw exception
+                    }
+                }.map { preferences ->
+                    val email = preferences[Constants.Preferences.SIGNED_USER_EMAIL]
+                    SignInPreferences(email)
+                }
+
     override suspend fun signIn(
         email: String,
         password: String,
     ) {
         userDao.insertUser(UserEntity(email = email, password = password))
+        dataStore.edit { preferences ->
+            preferences[Constants.Preferences.SIGNED_USER_EMAIL] = email
+        }
     }
 
-    override suspend fun isUserSignedIn(email: String): Boolean = userDao.getUserByEmail(email) != null
+    override suspend fun getUserByEmail(email: String): UserEntity? = userDao.getUserByEmail(email)
 }
--- app/src/main/java/com/evaluation/mangascope/domain/repository/AuthenticationRepository.kt
+++ app/src/main/java/com/evaluation/mangascope/domain/repository/AuthenticationRepository.kt
@@ -1,10 +1,16 @@
 package com.evaluation.mangascope.domain.repository
 
+import com.evaluation.mangascope.data.local.entity.UserEntity
+import com.evaluation.mangascope.domain.model.preferences.SignInPreferences
+import kotlinx.coroutines.flow.Flow
+
 interface AuthenticationRepository {
+    val signInPreferencesFlow: Flow<SignInPreferences>
+
     suspend fun signIn(
         email: String,
         password: String,
     )
 
-    suspend fun isUserSignedIn(email: String): Boolean
+    suspend fun getUserByEmail(email: String): UserEntity?
 }
