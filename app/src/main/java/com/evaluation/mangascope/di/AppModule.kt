2051120d0d77e281c5b6d338064ce488a2a84707
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-30 17:39:03 +0530

    feat: Add dependency injection module.
    - Create `AppModule` to inject dependencies using Koin.
    - Define dependencies for Room database, data store, repositories, use cases, and view models.

---
+++ app/src/main/java/com/evaluation/mangascope/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.evaluation.mangascope.di
+
+import androidx.room.Room
+import com.evaluation.mangascope.data.local.database.AppDatabase
+import com.evaluation.mangascope.data.repository.AuthenticationRepositoryImpl
+import com.evaluation.mangascope.data.repository.dataStore
+import com.evaluation.mangascope.domain.repository.AuthenticationRepository
+import com.evaluation.mangascope.domain.usecase.IsUserSignedInUseCase
+import com.evaluation.mangascope.domain.usecase.SignInUseCase
+import com.evaluation.mangascope.presentation.main.MainViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val appModule =
+    module {
+        single {
+            val database =
+                Room
+                    .databaseBuilder(
+                        androidContext(),
+                        AppDatabase::class.java,
+                        "manga_scope_db",
+                    ).build()
+            database.userDao()
+        }
+        single { androidContext().dataStore }
+        singleOf(::AuthenticationRepositoryImpl) { bind<AuthenticationRepository>() }
+        singleOf(::SignInUseCase)
+        singleOf(::IsUserSignedInUseCase)
+        viewModelOf(::MainViewModel)
+    }
