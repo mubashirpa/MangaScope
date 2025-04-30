0ed6ec3646db4e1ce30498fe141dc86b0ec66bad
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-30 16:58:13 +0530

    Refactor: Improve edge-to-edge behavior and implement splash screen.
    - Implement a splash screen using `installSplashScreen`.
    - Configure `enableEdgeToEdge` to support transparent status and navigation bars, handling dark mode dynamically.

--- app/src/main/java/com/evaluation/mangascope/presentation/main/MainActivity.kt
+++ app/src/main/java/com/evaluation/mangascope/presentation/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.evaluation.mangascope.presentation.main
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,13 +12,28 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.evaluation.mangascope.navigation.MangaScopeNavHost
+import com.evaluation.mangascope.navigation.Route
+import com.evaluation.mangascope.presentation.components.LoadingScreen
 import com.evaluation.mangascope.presentation.theme.MangaScopeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        enableEdgeToEdge(
+            statusBarStyle =
+                SystemBarStyle.auto(
+                    Color.TRANSPARENT,
+                    Color.TRANSPARENT,
+                    detectDarkMode = { true },
+                ),
+        )
+
         setContent {
             MangaScopeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
