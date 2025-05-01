ffc62e552ac90a075f5b46f90d408b6e84e7d358
Mubashir P A <mubashirpa2002@gmail.com>
2025-05-01 13:45:05 +0530

    feat: Add Manga, CacheMetadata, and RemoteKey entities to AppDatabase

--- app/src/main/java/com/evaluation/mangascope/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/evaluation/mangascope/data/local/database/AppDatabase.kt
@@ -2,10 +2,27 @@ package com.evaluation.mangascope.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.evaluation.mangascope.data.local.dao.CacheMetadataDao
+import com.evaluation.mangascope.data.local.dao.MangaDao
+import com.evaluation.mangascope.data.local.dao.RemoteKeyDao
 import com.evaluation.mangascope.data.local.dao.UserDao
+import com.evaluation.mangascope.data.local.entity.CacheMetadataEntity
+import com.evaluation.mangascope.data.local.entity.MangaEntity
+import com.evaluation.mangascope.data.local.entity.RemoteKeyEntity
 import com.evaluation.mangascope.data.local.entity.UserEntity
 
-@Database(entities = [UserEntity::class], version = 1)
+@Database(
+    entities = [
+        UserEntity::class, MangaEntity::class, CacheMetadataEntity::class, RemoteKeyEntity::class,
+    ],
+    version = 1,
+)
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun cacheMetadataDao(): CacheMetadataDao
+
+    abstract fun mangaDao(): MangaDao
+
+    abstract fun remoteKeyDao(): RemoteKeyDao
+
     abstract fun userDao(): UserDao
 }
