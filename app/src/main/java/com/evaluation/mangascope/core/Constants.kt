926d82cc8900f1f72d274ebb96cf336842687682
Mubashir P A <mubashirpa2002@gmail.com>
2025-05-01 13:48:30 +0530

    feat: Add Mangaverse API base URL constant

--- app/src/main/java/com/evaluation/mangascope/core/Constants.kt
+++ app/src/main/java/com/evaluation/mangascope/core/Constants.kt
@@ -3,6 +3,8 @@ package com.evaluation.mangascope.core
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object Constants {
+    const val MANGAVERSE_API_BASE_URL = "https://mangaverse-api.p.rapidapi.com"
+
     object Preferences {
         val SIGNED_USER_EMAIL = stringPreferencesKey("signed_user_email")
     }
